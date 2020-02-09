effaf165bdba2f168cabf406eecd55f440e2f5a4
Cristian Simionescu <cristian@nexusmedia.ro>
2020-02-10 00:40:30 +0200

    Idk a bit cleaner but still too much memory and poor performance

--- robustness.py
+++ robustness.py
@@ -2,9 +2,12 @@ import torch
 import torch.nn as nn
 from torch.nn import Parameter
 import torchvision
+from torch.utils.data.sampler import SubsetRandomSampler
 import torchvision.transforms as transforms
 from models import *
 from collections import OrderedDict
+import os
+import pickle
 
 """
 Adversarial Attack Options: fgsm, bim, mim, pgd
@@ -61,15 +64,14 @@ def attack(model, criterion, img, label, eps, attack_type, iters, clean_clean_im
 
     for j in range(iterations):
         outputs = None
-        adv_aux = adv * (1 - aug_test_lambda)
         if aug_test is None:
             out_adv = model(normalize(adv.clone()))
             loss = criterion(out_adv, label)
             loss.backward()
         else:
+            adv_aux = adv * (1.0 - aug_test_lambda)
             for i in range(aug_test):  # TODO Check why this uses so much memory... it ain't normal fam
-                aug_noise = clean_clean_img[torch.randperm(label.size(0))]
-                adv_aux = adv_aux + aug_test_lambda * aug_noise
+                adv_aux = adv_aux + aug_test_lambda * clean_clean_img[torch.randperm(label.size(0))]
                 out = model(normalize(adv_aux))
                 if outputs is None:
                     outputs = out
@@ -108,7 +110,8 @@ def attack(model, criterion, img, label, eps, attack_type, iters, clean_clean_im
 def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    state_dict = torch.load(baseline, map_location=device)
+    # state_dict = torch.load(baseline, map_location=device)
+    state_dict = torch.load(robust_model, map_location=device)
 
     model = PreResNet(101)
     if torch.cuda.is_available():
@@ -128,10 +131,31 @@ def main():
     train_set = torchvision.datasets.CIFAR10(root='../storage', train=True, download=True, transform=transform_test)
     train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
 
-    testset = torchvision.datasets.CIFAR10(root='../storage', train=False,
-                                           download=True, transform=transform_test)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, pin_memory=True,
-                                              shuffle=False, num_workers=4)
+
+    testset = torchvision.datasets.CIFAR10(root='../storage', train=False,download=True, transform=transform_test)
+
+    subset = 200
+    filename = "subset_indices/subset_balanced_test_CIFAR10_{}.data".format(subset)
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            subset_indices = pickle.load(f)
+    else:
+        subset_indices = []
+        per_class = subset // num_classes
+        targets = torch.tensor(testset.targets)
+        for i in range(num_classes):
+            idx = (targets == i).nonzero().view(-1)
+            perm = torch.randperm(idx.size(0))[:per_class]
+            subset_indices += idx[perm].tolist()
+        if not os.path.isdir("subset_indices"):
+            os.makedirs("subset_indices")
+        with open(filename, 'wb') as f:
+            pickle.dump(subset_indices, f)
+    subset_indices = torch.LongTensor(subset_indices)
+    test_loader = torch.utils.data.DataLoader(dataset=testset, batch_size=batch_size,sampler=SubsetRandomSampler(subset_indices))
+
+
+    # test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, pin_memory=True,shuffle=False, num_workers=4)
 
     # Test time sum aug
     aux, _ = next(iter(train_loader))
@@ -145,24 +169,33 @@ def main():
     eps = 8 / 255  # Epsilon for Adversarial Attack
 
     # Clean accuracy:91.710%   Adversarial accuracy:16.220%
+    total = 0
     for idx, (img, label) in enumerate(test_loader):
         img, label = img.to(device), label.to(device)
+        total += img.size(0)
         if aug_test is not None:
             clean_img = normalize(img.clone().detach())
-            outputs = torch.zeros_like(model(clean_img))
+            outputs = None
             for i in range(aug_test):
-                aug_data = clean_img * (1 - aug_test_lambda) + aug_test_lambda * clean_clean_img_normalized[torch.randperm(label.size(0))]
-                outputs += model(aug_data).detach()
+                aug_data = clean_img * (1.0 - aug_test_lambda) + aug_test_lambda * clean_clean_img_normalized[torch.randperm(label.size(0))]
+                if outputs is None:
+                    outputs = model(aug_data).detach()
+                else:
+                    outputs += model(aug_data).detach()
             output = outputs / aug_test
+            
             clean_acc += torch.sum(output.argmax(dim=-1) == label).item()
 
             adv = attack(model, criterion, img, label, eps=eps, attack_type='fgsm', iters=10, clean_clean_img=clean_clean_img)
             adv_img = normalize(adv.clone().detach())
 
-            outputs = torch.zeros_like(model(adv_img))
+            outputs = None
             for i in range(aug_test):
-                aug_data = adv_img * (1 - aug_test_lambda) + aug_test_lambda * clean_clean_img_normalized[torch.randperm(label.size(0))]
-                outputs += model(aug_data).detach()
+                aug_data = adv_img * (1.0 - aug_test_lambda) + aug_test_lambda * clean_clean_img_normalized[torch.randperm(label.size(0))]
+                if outputs is None:
+                    outputs = model(aug_data).detach()
+                else:
+                    outputs += model(aug_data).detach()
             output = outputs / aug_test
             adv_acc += torch.sum(output.argmax(dim=-1) == label).item()
         else:
@@ -170,7 +203,7 @@ def main():
             adv = attack(model, criterion, img, label, eps=eps, attack_type='fgsm', iters=10)
             adv_acc += torch.sum(model(normalize(adv.clone().detach())).argmax(dim=-1) == label).item()
         print('Batch: {0}'.format(idx))
-    print('Clean accuracy:{0:.3%}\t Adversarial accuracy:{1:.3%}'.format(clean_acc / len(testset), adv_acc / len(testset)))
+    print('Clean accuracy:{0:.3%}\t Adversarial accuracy:{1:.3%}'.format(clean_acc / total, adv_acc / total))
 
     # FGSM:
     # Baseline: Clean 94.52, Adv 24.08
