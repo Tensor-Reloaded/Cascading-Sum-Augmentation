1619f4980a1e87b58ce7ce8b3d369a7bb54828ef
Cosmin Pascaru <cosmin.pascaru2@gmail.com>
2020-07-01 11:32:34 +0300

    Fix wrong mean, std; Fix wrong model.train() before attack.

--- robustness.py
+++ robustness.py
@@ -1,3 +1,4 @@
+import re
 import torch
 import torch.nn as nn
 from torch.nn import Parameter
@@ -12,19 +13,27 @@ import pickle
 """
 Adversarial Attack Options: fgsm, bim, mim, pgd
 """
-num_classes = 10
-batch_size = 128
+ATTACKS = [
+    'fgsm',
+    'bim', 'mim', 'pgd',
+]
+
+NUM_CLASSES = 10
+BATCH_SIZE = 256
 
 
 # Loading Trained Model
-baseline = 'runs/Baseline/model_286_94.97.pth'
-robust_model = 'runs/PreResNet101 K=6 full gradual cos/model_505_94.43.pth'
-# robust_model= 'runs/PreResNet101 K=6 full gradual cos/model_300_0.pth'
+# baseline = 'runs/Baseline/model_286_94.97.pth'
+# robust_model = 'runs/PreResNet101 K=6 full gradual cos/model_505_94.43.pth'
 
+# baseline
+
+OUT_CSV = 'results_cifar=subset_lvl=model.csv'
+MODELS = []
 
 # Mean and Standard Deiation of the Dataset
-mean = [0.4914, 0.4822, 0.4465]
-std = [0.2023, 0.1994, 0.2010]
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
 
 mean = torch.tensor(mean).to("cuda")
 std = torch.tensor(std).to("cuda")
@@ -39,13 +48,15 @@ def un_normalize(t):
     return ret
 
 
-aug_test = 64
-aug_test_lambda = 0.5
+# aug_test = 64
+# aug_test_lambda = 0.5
+aug_test = None
+aug_test_lambda = None
 
 
 # Attacking Images batch-wise
 def attack(model, criterion, img, label, eps, attack_type, iters, clean_clean_img=None):
-    model.train()
+    assert not model.training
 
     adv = img.clone().detach()
     adv = Parameter(adv, requires_grad=True)
@@ -103,18 +114,24 @@ def attack(model, criterion, img, label, eps, attack_type, iters, clean_clean_im
 
         adv.grad.data.zero_()
 
-    model.eval()
     return adv.detach()
 
 
-def main():
+def run(model_path, attack_type):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # state_dict = torch.load(baseline, map_location=device)
-    state_dict = torch.load(robust_model, map_location=device)
+    if not model_path.endswith('.pth'):
+        model_files = list(filter(lambda f: f.endswith('.pth') or f.endswith('.pts'), os.listdir(model_path)))
+        assert len(model_files) == 1
+        model_path = os.path.join(model_path, model_files[0])
+    state_dict = torch.load(model_path, map_location=device)
 
-    model = PreResNet(101)
-    if torch.cuda.is_available():
+    model_name = re.findall(r'separated_runs\\[^\\]+\\', model_path)[0].split('\\')[1]
+    print(attack_type, model_name)
+
+    model = PreResNet(56)
+    if torch.cuda.is_available() and torch.cuda.device_count() > 1:
         new_state_dict = {}
         for key, value in state_dict.items():
             new_key = "module." + key
@@ -128,39 +145,42 @@ def main():
 
     # Loading Test Data (Un-normalized)
     transform_test = transforms.Compose([transforms.ToTensor(), ])
-    train_set = torchvision.datasets.CIFAR10(root='../storage', train=True, download=True, transform=transform_test)
-    train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
+    if aug_test is not None:
+        train_set = torchvision.datasets.CIFAR10(root='../storage', train=True, download=True, transform=transform_test)
+        train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
 
 
     testset = torchvision.datasets.CIFAR10(root='../storage', train=False,download=True, transform=transform_test)
 
-    subset = 200
-    filename = "subset_indices/subset_balanced_test_CIFAR10_{}.data".format(subset)
-    if os.path.isfile(filename):
-        with open(filename, 'rb') as f:
-            subset_indices = pickle.load(f)
+    subset = None
+    if subset:
+        filename = "subset_indices/subset_balanced_test_CIFAR10_{}.data".format(subset)
+        if os.path.isfile(filename):
+            with open(filename, 'rb') as f:
+                subset_indices = pickle.load(f)
+        else:
+            subset_indices = []
+            per_class = subset // NUM_CLASSES
+            targets = torch.tensor(testset.targets)
+            for i in range(NUM_CLASSES):
+                idx = (targets == i).nonzero().view(-1)
+                perm = torch.randperm(idx.size(0))[:per_class]
+                subset_indices += idx[perm].tolist()
+            if not os.path.isdir("subset_indices"):
+                os.makedirs("subset_indices")
+            with open(filename, 'wb') as f:
+                pickle.dump(subset_indices, f)
+        subset_indices = torch.LongTensor(subset_indices)
+        sampler = SubsetRandomSampler(subset_indices)
     else:
-        subset_indices = []
-        per_class = subset // num_classes
-        targets = torch.tensor(testset.targets)
-        for i in range(num_classes):
-            idx = (targets == i).nonzero().view(-1)
-            perm = torch.randperm(idx.size(0))[:per_class]
-            subset_indices += idx[perm].tolist()
-        if not os.path.isdir("subset_indices"):
-            os.makedirs("subset_indices")
-        with open(filename, 'wb') as f:
-            pickle.dump(subset_indices, f)
-    subset_indices = torch.LongTensor(subset_indices)
-    test_loader = torch.utils.data.DataLoader(dataset=testset, batch_size=batch_size,sampler=SubsetRandomSampler(subset_indices))
+        sampler = None
+    test_loader = torch.utils.data.DataLoader(dataset=testset, batch_size=BATCH_SIZE, sampler=sampler, num_workers=2)
 
-
-    # test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, pin_memory=True,shuffle=False, num_workers=4)
-
-    # Test time sum aug
-    aux, _ = next(iter(train_loader))
-    clean_clean_img = aux.clone().detach().to(device)
-    clean_clean_img_normalized = normalize(aux.clone().detach().to(device))
+    if aug_test is not None:
+        # Test time sum aug
+        aux, _ = next(iter(train_loader))
+        clean_clean_img = aux.clone().detach().to(device)
+        clean_clean_img_normalized = normalize(aux.clone().detach().to(device))
 
     # Loss Criteria
     criterion = nn.CrossEntropyLoss()
@@ -186,7 +206,7 @@ def main():
             
             clean_acc += torch.sum(output.argmax(dim=-1) == label).item()
 
-            adv = attack(model, criterion, img, label, eps=eps, attack_type='fgsm', iters=10, clean_clean_img=clean_clean_img)
+            adv = attack(model, criterion, img, label, eps=eps, attack_type=attack_type, iters=10, clean_clean_img=clean_clean_img)
             adv_img = normalize(adv.clone().detach())
 
             outputs = None
@@ -199,28 +219,25 @@ def main():
             output = outputs / aug_test
             adv_acc += torch.sum(output.argmax(dim=-1) == label).item()
         else:
-            clean_acc += torch.sum(model(normalize(img.clone().detach())).argmax(dim=-1) == label).item()
-            adv = attack(model, criterion, img, label, eps=eps, attack_type='fgsm', iters=10)
-            adv_acc += torch.sum(model(normalize(adv.clone().detach())).argmax(dim=-1) == label).item()
-        print('Batch: {0}'.format(idx))
-    print('Clean accuracy:{0:.3%}\t Adversarial accuracy:{1:.3%}'.format(clean_acc / total, adv_acc / total))
+            with torch.no_grad():
+                clean_acc += torch.sum(model(normalize(img.clone().detach())).argmax(dim=-1) == label).item()
+            adv = attack(model, criterion, img, label, eps=eps, attack_type=attack_type, iters=10)
+            with torch.no_grad():
+                adv_acc += torch.sum(model(normalize(adv.detach())).argmax(dim=-1) == label).item()
 
-    # FGSM:
-    # Baseline: Clean 94.52, Adv 24.08
-    # K=6-1 aug=0 lambda=0.0: Clean 93.94, Adv 54.0
+        # print('Batch: {0}'.format(idx))
 
-    # BIM
-    # Baseline: Clean 94.52, Adv 24.08
-    # K=6-1 aug=0 lambda=0.0: Clean 93.94, Adv 54.0
+    clean_acc /= total
+    adv_acc /= total
+    print('Clean accuracy:{0:.3%}\t Adversarial accuracy:{1:.3%}'.format(clean_acc, adv_acc))
+    with open(OUT_CSV, 'a') as f:
+        f.write(f'{model_name},{attack_type},{clean_acc},{adv_acc}\n')
 
-    # MIN:
-    # Baseline: Clean 94.52, Adv 0.0
-    # K=6-1 aug=0 lambda=0.0: Clean 93.94, Adv 9.19
 
-    # PGD
-    # Baseline: Clean 94.52, Adv 0.0
-    # K=6-1 aug=0 lambda=0.0: PGD Clean 93.94, Adv 3.22
-
+def main():
+    for attack_type in ATTACKS:
+        for model_path in MODELS:
+            run(model_path, attack_type)
 
 if __name__ == '__main__':
     main()
